80106bfad9c80c68161927f276a8169bbd9c2705
Flexomatic81 <hadzic@tradeo.de>
2025-12-15 21:06:28 +0100

    Admin Integration Bugfix

--- dashboard/src/routes/admin.js
+++ dashboard/src/routes/admin.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
 const { requireAuth, requireAdmin } = require('../middleware/auth');
 const { getPool } = require('../config/database');
 const projectService = require('../services/project');
